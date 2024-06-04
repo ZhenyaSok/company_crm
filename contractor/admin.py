10b25b65cb5b5925cd8d017ba4d1f95aa2d79b14
Jenny <kapitan.kub@gmail.com>
2024-06-05 00:14:35 +0500

    Зарегестрированы модели Account, Contractor

--- contractor/admin.py
+++ contractor/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from contractor.models import Account, Contractor
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    """Админ-панель счетов"""
+    list_display = ('id', 'name', 'inn', 'number_account')
+
+
+@admin.register(Contractor)
+class ContractorAdmin(admin.ModelAdmin):
+    """Админ-панель поставщик"""
+    list_filter = ['city', 'level']
+    list_display = ['name', 'city', 'debt', 'level']
+
