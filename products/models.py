61e389f85352dff5079fe796205caf1c14d1ef10
Jenny <kapitan.kub@gmail.com>
2024-06-04 15:11:37 +0500

    Созданы модели категорий и продуктов

--- products/models.py
+++ products/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from config import settings
+
+NULLABLE = {'null': True, 'blank': True}
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Категория')
+    description = models.TextField(**NULLABLE)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование товара')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    photo = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Изображение')
+    price = models.FloatField(default=0, verbose_name='Цена за штуку')
+    overview = models.TextField(**NULLABLE, verbose_name='Описание')
+    created = models.DateTimeField(auto_now_add=True)
+
+
+
+
+    contractor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='пользователь')
+    is_published = models.BooleanField(default=False, verbose_name='статус публикации')
+
+
+    def __str__(self):
+        return f'{self.name} ({self.price})'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ('price',)
+
+
+class Model(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Наименование товара')
+    num_version = models.IntegerField(verbose_name='номер модели')
+    name_version = models.CharField(max_length=100, verbose_name='название модели')
+    version_flug = models.BooleanField(verbose_name='признак модели', default=False)
+
+    def __str__(self):
+        return f'{self.product} ({self.name_version})'
+
+
+
+    class Meta:
+        verbose_name = 'Модель'
+        verbose_name_plural = 'Модели'
