722cce869b308cff7f36e89d2eb7eaca98895d34
Jenny <kapitan.kub@gmail.com>
2024-06-06 00:26:50 +0500

    зарегестрированы урлы приложений Contractor, Products, djoser

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,33 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf import settings
 from django.conf.urls.static import static
-from config import settings
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products', include('products.urls')),
+    path('api/', include('products.urls')),
+    path('api/', include('contractor.urls')),
+
+    path("api/", include("users.urls")),
+
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 
 
 ]
