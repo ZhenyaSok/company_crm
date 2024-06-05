d0f5310cce6b084295e337735261429dfed99826
Jenny <kapitan.kub@gmail.com>
2024-06-06 00:32:08 +0500

    Админка по тз модель поставщика (Contractor)

--- contractor/admin.py
+++ contractor/admin.py
@@ -4,6 +4,7 @@ from contractor import models
 from contractor.models import Account, Contractor, Address
 
 
+
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
     """Админ-панель счетов"""
@@ -13,7 +14,16 @@ class AccountAdmin(admin.ModelAdmin):
 @admin.register(Contractor)
 class ContractorAdmin(admin.ModelAdmin):
     """Админ-панель поставщик"""
-    list_display = ['name', 'address', 'debt', 'provider', 'products_names']
+
+    list_display = ['name', 'address', 'bank_account', 'debt', 'provider', 'products_names']
+    fields = ['name', 'address', 'bank_account', ('provider', 'levels'), 'products', 'debt']  # TODO сделать задолжность чтоб считалась и не исправ
+    list_filter = ['address']   # TODO решить с городами, создать базу на русском, что вставить в фильтр?
+    search_fields = ['name']
+    actions = ['clear_arrears']
+
+    @admin.action(description="Очищение задолженности")
+    def clear_arrears(modeladmin, request, queryset):
+        queryset.update(arrears=0)
 
 
 @admin.register(Address)
