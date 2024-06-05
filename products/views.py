3fb63621e01eaa0f87dee178ea48cf75ff0aac58
Jenny <kapitan.kub@gmail.com>
2024-06-06 00:33:56 +0500

    View на модель товаров и категорий товаров готовы

--- products/views.py
+++ products/views.py
@@ -1,19 +1,19 @@
 from rest_framework import viewsets, permissions
 
 from products.models import Product, Category
-from products.serializers import ProductSerializers, CategorySerializers
+from products.serializers import ProductSerializer, CategorySerializer
 
 
 class IsActiveUser(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated and request.user.is_active
 class ProductViewSet(viewsets.ModelViewSet):
-    serializer_class = ProductSerializers
+    serializer_class = ProductSerializer
     queryset = Product.objects.all()
     permission_classes = [IsActiveUser]
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
-    serializer_class = CategorySerializers
+    serializer_class = CategorySerializer
     queryset = Category.objects.all()
     permission_classes = [IsActiveUser]
