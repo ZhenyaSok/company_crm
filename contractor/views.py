1072dc1a5aacf1e4a5b6455adbbdae71664bbbfa
Jenny <kapitan.kub@gmail.com>
2024-06-06 00:33:14 +0500

    View на модель поставщика (Contractor), фильтры и пермишены готовы

--- contractor/views.py
+++ contractor/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 
-# Create your views here.
+from contractor.models import Contractor
+from contractor.serializers import ContractorSerializer
+from products.views import IsActiveUser
+
+
+class ContractorViewSet(viewsets.ModelViewSet):
+    serializer_class = ContractorSerializer
+    queryset = Contractor.objects.all()
+    filter_backends = [SearchFilter]  # фильтр поиска по стране
+    filterset_fields = ['country']
+    permission_classes = [IsActiveUser]
