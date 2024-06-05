7f1ea9b0c01250e3d943e4675803b74188cf4648
Jenny <kapitan.kub@gmail.com>
2024-06-06 00:21:47 +0500

    Отредактирован метод __str__, добавлено поле модель продукта

--- products/models.py
+++ products/models.py
@@ -25,7 +25,8 @@ class Product(models.Model):
     photo = models.ImageField(upload_to='catalog/', verbose_name='Изображение', **NULLABLE)
     price = models.FloatField(default=0, verbose_name='Цена за штуку')
     overview = models.TextField(verbose_name='Описание', **NULLABLE)
-    created = models.DateTimeField(auto_now_add=True)
+    model_product = models.CharField(max_length=100, verbose_name='Модель')
+    created_at = models.DateField(auto_now_add=True)
 
     manager = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,
                                 verbose_name='пользователь', **NULLABLE)
@@ -33,25 +34,9 @@ class Product(models.Model):
 
 
     def __str__(self):
-        return f'{self.name} ({self.price})'
+        return f'{self.name} {self.created_at} {self.model_product} ({self.price})'
 
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
         ordering = ('price',)
-
-
-class Model(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Наименование товара')
-    num_version = models.IntegerField(verbose_name='номер модели')
-    name_version = models.CharField(max_length=100, verbose_name='название модели')
-    version_flug = models.BooleanField(verbose_name='признак модели', default=False)
-
-    def __str__(self):
-        return f'{self.product} ({self.name_version})'
-
-
-
-    class Meta:
-        verbose_name = 'Модель'
-        verbose_name_plural = 'Модели'
