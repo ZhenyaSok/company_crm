070d6b9fb0b6a62257771596305c356e14830eb9
Jenny <kapitan.kub@gmail.com>
2024-06-06 00:29:44 +0500

    настроены сериалайзеры для отражения моделей Сontractor, Product, Categoty

---
+++ contractor/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from contractor.models import Contractor
+from products.models import Product
+
+
+class ContractorSerializer(serializers.ModelSerializer):
+    address = serializers.CharField(source="address.get_complete_address", read_only=True)
+    list_products = serializers.SerializerMethodField()
+    read_only_fields = ('debt',)
+
+    def get_list_products(self, contractor):
+        """Метод выводит все товары которые может реализовать поставщик"""
+        list = []
+        for product in Product.objects.filter(contractor=contractor):
+            a = f"{product.name} модель {product.model_product}, цена {product.price}, дата выхода {product.created_at}"
+            list.append(a)
+        return list
+        # return [product.name for product in Product.objects.filter(contractor=contractor)]
+
+    class Meta:
+        model = Contractor
+        fields = '__all__'
--- products/serializers.py
+++ products/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from products.models import Product, Category
 
 
-class ProductSerializers(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
 
 
-class CategorySerializers(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
